f0a4668291b440d274b9698c3ef1021c5c5e3cbb
Musab Popatia <mpopatia@qatar.cmu.edu>
2016-02-07 18:59:59 +0300

    First commit

---
+++ steps/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello!")
+
+
+def login_page(request):
+    return render(request,'steps/login_page.html')
